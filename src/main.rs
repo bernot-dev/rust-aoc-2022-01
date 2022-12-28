ce24176316b15c09da9574f7d2851699e28d2955
Adam Bernot <bernot@google.com>
2022-12-28 18:39:26 +0000

    part 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
+use std::collections::BinaryHeap;
+
 fn main() {
+    let input: &str = include_str!("../input.txt");
+    println!("Part 1 Solution: {}", part1(input));
+    println!("Part 2 Solution: {}", part2(input));
+}
+
+fn part1(input: &str) -> u64 {
     let mut max: u64 = 0;
-    for group in include_str!("../input.txt").split("\n\n") {
+    for group in input.split("\n\n") {
         let mut sum: u64 = 0;
         for line in group.lines() {
             let value: u64 = line.parse().unwrap();
@@ -10,5 +18,25 @@ fn main() {
             max = sum
         }
     }
-    println!("{max}")
+    max
+}
+
+fn part2(input: &str) -> u64 {
+    let mut heap = BinaryHeap::new();
+    let mut current_group: u64 = 0;
+    for line in input.lines() {
+        if line == "" {
+            heap.push(current_group);
+            current_group = 0;
+            continue;
+        }
+        current_group += line.parse::<u64>().unwrap();
+    }
+
+    let mut solution: u64 = 0;
+    for _ in 0..3 {
+        let val = heap.pop().unwrap();
+        solution += val;
+    }
+    solution
 }
